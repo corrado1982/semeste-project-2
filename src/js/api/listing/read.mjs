eb1295b10f71f4d78c2845549bb1f84221254c0e
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-09-07 18:09:50 +0200

    displaying bids on the listings page

--- src/js/api/listing/read.mjs
+++ src/js/api/listing/read.mjs
@@ -5,22 +5,25 @@ const cardTitle = document.querySelector("#itemTitle");
 const cardBids = document.querySelector("#bids");
 const cardExpire = document.querySelector("#remainingTime");
 const cardDescription = document.querySelector("#description");
+const cardBidsContainer = document.querySelector("#bid-details");
 // const cardAppendPicture = document.querySelector("#card-body");
 //til here
 
 const itemContainer = document.querySelector("#listItem");
 const cardsContainer = document.querySelector("#cardsContainer");
 
+const bidsUrl = "?_bids=true";
 const listingsUrlEnd = "/listings/";
 const listingsUrl = BASE_URL + listingsUrlEnd;
 
 export async function readListings() {
-  const response = await fetch(listingsUrl);
+  const response = await fetch(listingsUrl + bidsUrl);
   const result = await response.json();
   //   console.log(result);
 
   result.forEach((element) => {
     console.log(element);
+    console.log(element._count.bids);
 
     cardsContainer.innerHTML += `
     <div  class="col-6 m-5 card" style="width: 18rem">
@@ -29,16 +32,16 @@ export async function readListings() {
           <img src="${element.media[0]}" id="itemImage" class="card-img-top" alt="image here" />
             <h5 id="itemTitle" class="card-title">${element.title}</h5>
             <div class="row">
-              <p class="card-text col-6">price now</p>
+              <p class="card-text col-6">Bids</p>
               <p
                 id="actualPrice"
                 class="card-text col-6 d-flex justify-content-end"
-              >
-                1000
+              >${element._count.bids}
+                
               </p>
             </div>
             <div class="row">
-              <p class="card-text col-6">contdown</p>
+              <p class="card-text col-6">Expiring</p>
               <p
                 id="remainingTime"
                 class="card-text col-6 d-flex justify-content-end"
@@ -59,11 +62,23 @@ const id = params.get("id");
 // const id = "dc4b2b25-c51a-4e76-a334-148d4af9f8e2";
 
 export async function readListing() {
-  const response = await fetch(listingsUrl + id);
+  const response = await fetch(listingsUrl + id + bidsUrl);
   const result = await response.json();
+
+  result.bids.forEach((bidDetail) => {
+    console.log(bidDetail.bidderName);
+    console.log(bidDetail.amount);
+    cardBidsContainer.innerHTML += `
+    <div class="d-flex justify-content-between">
+    <p>${bidDetail.bidderName}</p>
+    <p>${bidDetail.amount}</p>
+    </div>`;
+  });
   console.log(id);
   console.log(result);
+  console.log(result.bids);
   //prove da qui
+
   cardTitle.innerHTML = `${result.title}`;
   cardBids.innerHTML = `${result._count.bids}`;
   cardExpire.innerHTML = `${result.endsAt}`;
@@ -75,50 +90,51 @@ export async function readListing() {
 
   img.src = result.media[0];
   img.className = "card-img-top";
+  img.alt = `image of ${result.title}`;
 
   const src = document.getElementById("card-body");
 
   src.appendChild(img);
 
   //fino qui
-  itemContainer.innerHTML = `
-    <div  class="col-6 m-auto card">
+  //   itemContainer.innerHTML = `
+  //     <div  class="col-6 m-auto card">
 
-            <div class="card-body">
-            <img src="${result.media[0]}" id="itemImage" class="card-img-top" alt="image of ${result.title}" />
-              <h5 id="itemTitle" class="card-title">${result.title}</h5>
+  //             <div class="card-body">
+  //             <img src="${result.media[0]}" id="itemImage" class="card-img-top" alt="image of ${result.title}" />
+  //               <h5 id="itemTitle" class="card-title">${result.title}</h5>
 
-              <div class="row">
-                <p class="card-text col-6">Expiring</p>
-                <p
-                  id="remainingTime"
-                  class="card-text col-6 d-flex justify-content-end"
-                >
-                ${result.endsAt}
-                </p>
-              </div>
-              <div class="row">
-                <p class="card-text col-6">Bids</p>
-                <p
-                  id="actualPrice"
-                  class="card-text col-6 d-flex justify-content-end"
-                >
-                ${result._count.bids}
-                </p>
-              </div>
-              <div class="row">
-                <p class="card-text col-6">Bid Amount</p>
-                <p
-                  id="actualPrice"
-                  class="card-text col-6 d-flex justify-content-end"
-                >
-                Amount
-                </p>
-              </div>
-              <div class="d-grid gap-2 col-3 mx-auto">
-              <a href="#" class="btn btn-primary">Bid Now</a>
-            </div>
-            </div>
-          </div>
-      `;
+  //               <div class="row">
+  //                 <p class="card-text col-6">Expiring</p>
+  //                 <p
+  //                   id="remainingTime"
+  //                   class="card-text col-6 d-flex justify-content-end"
+  //                 >
+  //                 ${result.endsAt}
+  //                 </p>
+  //               </div>
+  //               <div class="row">
+  //                 <p class="card-text col-6">Bids</p>
+  //                 <p
+  //                   id="actualPrice"
+  //                   class="card-text col-6 d-flex justify-content-end"
+  //                 >
+  //                 ${result._count.bids}
+  //                 </p>
+  //               </div>
+  //               <div class="row">
+  //                 <p class="card-text col-6">Bid Amount</p>
+  //                 <p
+  //                   id="actualPrice"
+  //                   class="card-text col-6 d-flex justify-content-end"
+  //                 >
+  //                 Amount
+  //                 </p>
+  //               </div>
+  //               <div class="d-grid gap-2 col-3 mx-auto">
+  //               <a href="#" class="btn btn-primary">Bid Now</a>
+  //             </div>
+  //             </div>
+  //           </div>
+  //       `;
 }
