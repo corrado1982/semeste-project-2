843945105f0fd6be5c58ed05b5f97fa9f36cf457
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-09-09 00:04:57 +0200

    insert avatar on navigation

--- src/js/components/populateNav.mjs
+++ src/js/components/populateNav.mjs
@@ -2,6 +2,7 @@ import * as storage from "../storage/index.mjs";
 const profile = storage.load("profile");
 const displayPoints = document.querySelector("#points");
 const displayName = document.querySelector("#loggedName");
+const displayAvatar = document.querySelector("#image-avatar");
 
 const credits = profile.credits;
 const avatar = profile.avatar;
@@ -10,4 +11,7 @@ const userName = profile.name;
 export function populateNavigation() {
   displayPoints.innerHTML = credits;
   displayName.innerHTML = userName;
+  if (avatar) {
+    displayAvatar.innerHTML = `<img src="${avatar}" alt="image of ${userName} s Avata">`;
+  }
 }
