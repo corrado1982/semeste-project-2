b05ea47e7e93da9eb6740f435783a2daf92ac206
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-09-08 22:31:23 +0200

    cleaning code

--- src/js/api/listing/bid.mjs
+++ src/js/api/listing/bid.mjs
@@ -1,20 +1,15 @@
 import { BASE_URL } from "../constants.mjs";
 import * as storage from "../../storage/index.mjs";
 
-// import { authFetch } from "../authFetch.mjs";
-// const bidButton = document.querySelector("#bid-button");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
 const token = storage.load("token");
-console.log(token);
-// const method = "post";
+
 const method = "post";
-// const body = JSON.stringify({ amount: newBid }); //new bid
 
 export async function makeBid(bid) {
-  //pass new bid ?
   const bidUrl = BASE_URL + "/listings/" + id + "/bids";
 
   const response = await fetch(bidUrl, {
@@ -27,11 +22,4 @@ export async function makeBid(bid) {
   });
   console.log(response);
   location.reload();
-  //   console.log(body);
 }
-// const bidButton = document.querySelector("#bid-button");
-// export async function bidListener() {
-//   bidButton.addEventListener("click", () => {
-//     makeBid();
-//   });
-// }
