b8d959516ad276ca7f56584f76bd412917447f76
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-09-14 07:49:27 +0200

    fixed populateNav

--- src/js/components/populateNav.mjs
+++ src/js/components/populateNav.mjs
@@ -1,17 +1,20 @@
-// import * as storage from "../storage/index.mjs";
-// const profile = storage.load("profile");
-// const displayPoints = document.querySelector("#points");
-// const displayName = document.querySelector("#loggedName");
-// const displayAvatar = document.querySelector("#image-avatar");
+import * as storage from "../storage/index.mjs";
+const profile = storage.load("profile");
+const displayPoints = document.querySelector("#points");
+const displayName = document.querySelector("#loggedName");
+const displayAvatar = document.querySelector("#image-avatar");
 
 // const credits = profile.credits;
 // const avatar = profile.avatar;
 // const userName = profile.name;
 
-// export function populateNavigation() {
-//   displayPoints.innerHTML = credits;
-//   displayName.innerHTML = userName;
-//   if (avatar) {
-//     displayAvatar.innerHTML = `<img src="${avatar}" alt="image of ${userName} s Avata">`;
-//   }
-// }
+export async function populateNavigation() {
+  const credits = profile.credits;
+  const avatar = profile.avatar;
+  const userName = profile.name;
+  displayPoints.innerHTML = credits;
+  displayName.innerHTML = userName;
+  if (avatar) {
+    displayAvatar.innerHTML = `<img src="${avatar}" alt="image of ${userName} s Avata">`;
+  }
+}
